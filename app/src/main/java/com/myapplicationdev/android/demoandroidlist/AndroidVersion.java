9747530f364a3ef00159681c1602339a95db5f97
mari <21001096@myrp.edu.sg>
2022-07-21 12:10:29 +0800

    commit

---
+++ app/src/main/java/com/myapplicationdev/android/demoandroidlist/AndroidVersion.java
@@ -0,0 +1,41 @@
+package com.myapplicationdev.android.demoandroidlist;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class AndroidVersion {
+
+        private String name;
+        private String version;
+
+    public AndroidVersion(String name, String version) {
+        this.name = name;
+        this.version = version;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    @Override
+    public String toString() {
+        return  name + " : " + version;
+    }
+}
